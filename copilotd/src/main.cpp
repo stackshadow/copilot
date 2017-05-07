2b88cb0f792c9e2a4b8ce5e42e0ffebfdf0e91bf
stackshadow <stackshadow@evilbrain.de>
2017-05-07 19:34:41 +0000

    copilotd: Add command-line parameter - hostname can be set from command-line for test

--- copilotd/src/main.cpp
+++ copilotd/src/main.cpp
@@ -23,6 +23,7 @@ along with copilot.  If not, see <http://www.gnu.org/licenses/>.
 //#include "doDBDws.h"
 //#include "wsPlugins/doDBDPluginList.h"
 //#include "wsPlugins/doDBDConnections.h"
+#include <getopt.h>
 
 #include "core/etInit.h"
 
@@ -39,14 +40,55 @@ along with copilot.  If not, see <http://www.gnu.org/licenses/>.
 
 //#include <QtCore/QCoreApplication>
 
+static struct option options[] = {
+    { "help",       no_argument,        NULL, 'h' },
+    { "debug",      no_argument,        NULL, 'd' },
+    { "hostname",   required_argument,  NULL, 'n' },
+    { NULL, 0, 0, 0 }
+};
+
 int main( int argc, char *argv[] ){
 
     etInit(argc,(const char**)argv);
+    etDebugLevelSet( etID_LEVEL_WARNING );
 //    QCoreApplication a(argc, argv);
 
 // create the core which contains all services
     coCore*         newcoCore = new coCore();
     
+// parse options
+    int optionSelected = 0;
+    while( optionSelected >= 0 ) {
+        optionSelected = getopt_long(argc, argv, "", options, NULL);
+        if( optionSelected < 0 )
+            continue;
+
+        switch ( optionSelected ) {
+            case '?':
+                exit(1);
+                
+            case 'd':
+                etDebugLevelSet( etID_LEVEL_ALL );
+                break;
+
+            case 'h':
+                fprintf( stdout, "Usage: %s\n", argv[0] );
+                fprintf( stdout, "--help: Show this help\n" );
+                fprintf( stdout, "--hostname <hostname>: Set the hostname ( not detect it automatically )\n" );
+                exit(1);
+
+            case 'n':
+                printf ("Set hostname to '%s'\n", optarg);
+                newcoCore->setHostName( optarg );
+                break;
+        }
+
+
+    }
+
+
+
+
 // create services
     coreService*    newCoreService = new coreService();
 
