1384cbc40b05fda5b8f347cc8984a2b64a71753a
stackshadow <stackshadow@evilbrain.de>
2018-07-21 14:43:12 +0200

    Add sphinx/breathe/doxygen documentation functionality

---
+++ doc/sphinx/index.cpp
@@ -0,0 +1,23 @@
+.. copilotd documentation master file, created by
+   sphinx-quickstart on Sat Jul 21 11:33:13 2018.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to copilotd's documentation!
+====================================
+
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+.. doxygengroup:: lsslService
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
